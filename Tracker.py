6eeffe0c24e277d36f7c659e65f061f302b3a2a4
fa16np <fa16np@brocku.ca>
2020-03-30 17:13:42 -0400

    Bug Fix

--- Tracker.py
+++ Tracker.py
@@ -1,7 +1,8 @@
 import requests
 import csv
 
-response = requests.get("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv");
+response = requests.get("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv");
+
 parsed = response.content.decode('utf-8')
 csveed = csv.reader(parsed.splitlines(), delimiter=',')
 
